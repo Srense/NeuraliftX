f2a4146b0a4f62d9e524521203bccc20ef9fca04
Sohel Rizwan <sohel.rizwan@example.com>
2025-09-11 18:13:40 +0530

    fix

--- EduConnect/src/Components/Faculty.jsx
+++ EduConnect/src/Components/Faculty.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Admin.css"; // Use the universal theme CSS file for all dashboards!
 import logo from "../assets/Logo.png";
-import"./student.css";
+import"./Student.css";
 
 // ================= THEME SYNC HOOK ================
 function useGlobalTheme() {
